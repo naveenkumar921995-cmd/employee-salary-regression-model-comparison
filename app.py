1d6d50542d18d611c3e0e64da5862c7f439a316c
naveenkumar921995-cmd <naveenkumar921995@gmail.com>
2026-03-06 22:25:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,20 @@
+import streamlit as st
+import numpy as np
+from sklearn.linear_model import LinearRegression
+import pandas as pd
 
+dataset = pd.read_csv("data/emp_sal.csv")
+
+X = dataset.iloc[:, 1:2].values
+y = dataset.iloc[:, 2].values
+
+model = LinearRegression()
+model.fit(X,y)
+
+st.title("Employee Salary Prediction")
+
+level = st.slider("Position Level",1.0,10.0,5.0)
+
+prediction = model.predict([[level]])
+
+st.write("Predicted Salary:", prediction[0])
